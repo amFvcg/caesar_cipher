4f49d0746707a6c76bf212c9e1c09044b870d41a
amFvcg <amfvcg@tutanota.com>
2016-03-31 23:44:17 +0200

    [PY] Renamed to rot47, handled only change in ascii chars 33-126

---
+++ py/rot47.py
@@ -0,0 +1,18 @@
+#!/bin/env python3
+import sys
+
+
+def rot47(line):
+    return [chr(33+(ord(i)+47-33) % 94) if ord(i) > 32 and ord(i) < 127 else i
+            for i in line]
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        for filename in sys.argv[1:]:
+            with open(filename) as f:
+                for line in f:
+                    print(''.join(rot47(line)), end='')
+    else:
+        for line in sys.stdin:
+            print(''.join(rot47(line)), end='')
